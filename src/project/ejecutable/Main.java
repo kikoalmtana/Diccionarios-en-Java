5b888d6e281c8ef5bab83ebebe94be82ba7fb57f
kikoalmtana <kiko2005ulpgc@gmail.com>
2025-02-20 18:08:25 +0000

    clases main y diccionario

--- src/project/ejecutable/Main.java
+++ src/project/ejecutable/Main.java
@@ -1,3 +1,4 @@
+package project.ejecutable;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
---
+++ src/project/ejecutable/diccionario.java
@@ -0,0 +1,5 @@
+package project.ejecutable;
+
+public class diccionario {
+    
+}
