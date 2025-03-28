f2387e0d49eeb4528a2396adefbef2a1396567e4
kikoalmtana <kiko2005ulpgc@gmail.com>
2025-03-28 12:57:36 +0000

    Finalización clase Nodo.java

--- src/Nodo.java
+++ src/Nodo.java
@@ -1,33 +1,103 @@
-public class Nodo<K, V> {
+/**
+ * Clase que implementa un nodo con clave y valor.
+ * Posee un enlace simple para nodos que se encuentren en el mismo indice del diccionario.
+ * Posee un doble enlace para todos los nodos del diccionario, tiene como objetivo poder mantener el orden de inserción.
+ * 
+ * @param <K> Tipo de la clave
+ * @param <V> Tipo del valor
+ */
+public class Node<K, V> {
     private K key;
     private V value;
-    private Nodo<K, V> next;
-    private Nodo<K, V> prevDouble;
-    private Nodo<K, V> nextDouble;
+    public Node<K, V> next;
+    public Node<K, V> prevDouble;
+    public Node<K, V> nextDouble;
 
-    public Nodo(K key, V value) {
+    public Node(K key, V value) {
         this.key = key;
         this.value = value;
         this.next = null;
         this.prevDouble = null;
         this.nextDouble = null;
     }
+    
+    /**
+     * Método que devuelve la clave del nodo
+     * 
+     * @return key, clave del nodo
+     */
+     public K getKey() {
+        return key;
+    }
+    
+    /**
+     * Método que cambia el valor de la clave
+     * 
+     * @param K newKey, valor nuevo de la clave
+     */
+    public void setKey(K newKey) {
+        key = newKey;
+    }
+    
+    /**
+     * Método que devuelve el valor del nodo
+     * 
+     * @return value, clave del nodo
+     */
+     
+    public V getValue() {
+        return value;
+    }
+    
+    /**
+     * Método que cambia el valor del nodo
+     * 
+     * @param K newValue, valor nuevo
+     */
+     
+    public void setValue(V newValue) {
+        value = newValue;
+    }
+    
+    /**
+     * Método que devuelve la referencia al siguiente nodo del mismo índice
+     * 
+     * @return next, referencia al siguiente nodo por índice
+     */
 
-    public K getKey() { return key; }
-    public V getValue() { return value; }
-    public Nodo<K, V> getNext() { return next; }
-    public Nodo<K, V> getPrevDouble() { return prevDouble; }
-    public Nodo<K, V> getNextDouble() { return nextDouble; }
+    public Node<K, V> getNext() {
+        return next;
+    }
+    
+     /**
+     * Método que devuelve la referencia al nodo anterior que se haya insertado en el diccionario.
+     * 
+     * @return prevDouble, referencia al nodo previo por orden de inserción
+     */
+
+    public Node<K, V> getPrevDouble() {
+        return prevDouble;
+    }
+    
+     /**
+     * Método que devuelve la referencia al siguiente nodo que se haya insertado en el diccionario.
+     * 
+     * @return nextDouble, referencia al siguiente nodo por orden de inserción
+     */
+
+    public Node<K, V> getNextDouble() {
+        return nextDouble;
+    }
+    
+    /**
+     * Método que devuelve una String con la información del nodo
+     * 
+     * @return El valor de clave con su valor asociado
+     */
 
     @Override
     public String toString() {
-        String nextKey;
-        if (this.next != null && this.next.key != null) {
-            nextKey = this.next.key.toString();
-        } else {
-            nextKey = "null";
-        }
-        return "Key: " + key.toString() + ", Value: " + value.toString() + ", Next: " + nextKey;
+        if (this.value == null) return "Key: " + key.toString() + ", Value: null";
+        return "Key: " + key.toString() + ", Value: " + value.toString();
     }
 }
-
